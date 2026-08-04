kaze-hk/gotosocial

commit f20588d77899a2dcca70ab8da98295991af9bb66

[bugfix]: make GET /api/v1/admin/reports match Mastodon admin API (#4778)
Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

# Description

This pull request fixes the mismatch between GoToSocial implementation of the admin API and Mastodon *reference* implementation. I introduced the new `unresolved` parameter which I also suggested in a PR to Mastodon. ~~Will unmark as WIP when at home where I have a working GTS dev/testenv set up, I'm in a train rn.~~

closes #4378

## Checklist

Please put an x inside each checkbox to indicate that you've read and followed it: `[ ]` -> `[x]`

If this is a documentation change, only the first two checkboxes must be filled (you can delete the others if you want).

- [x] I/we have read the [GoToSocial contribution guidelines](https://codeberg.org/superseriousbusiness/gotosocial/src/branch/main/CONTRIBUTING.md).
- [x] I/we have not used so-called 'AI' to create the proposed changes.
- [ ] I/we have discussed the proposed changes already, either in an issue on the repository, or in the Matrix chat.
- [ ] I/we have performed a self-review of added code.
- [x] I/we have written code that is legible and maintainable by others.
- [x] I/we have commented the added code, particularly in hard-to-understand areas.
- [x] I/we have made any necessary changes to documentation.
- [x] I/we have added tests that cover new code.
- [x] I/we have run tests and they pass locally with the changes.
- [x] I/we have run `go fmt ./...` and `golangci-lint run`.

Reviewed-on: https://codeberg.org/superseriousbusiness/gotosocial/pulls/4778
Co-authored-by: nicole mikołajczyk <git@mkljczk.pl>
Co-committed-by: nicole mikołajczyk <git@mkljczk.pl>

internal/processing/admin/report.go
@@ -22,7 +22,6 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
-	"strconv"
 	"time"
 
 	"code.superseriousbusiness.org/gotosocial/internal/ap"
@@ -33,6 +32,7 @@ import (
 	"code.superseriousbusiness.org/gotosocial/internal/gtsmodel"
 	"code.superseriousbusiness.org/gotosocial/internal/messages"
 	"code.superseriousbusiness.org/gotosocial/internal/paging"
+	"code.superseriousbusiness.org/gotosocial/internal/util"
 )
 
 // ReportsGet returns reports stored on this
@@ -40,11 +40,25 @@ import (
 func (p *Processor) ReportsGet(
 	ctx context.Context,
 	account *gtsmodel.Account,
-	resolved *bool,
+	resolvedParam bool,
+	unresolvedParam bool,
 	accountID string,
 	targetAccountID string,
 	page *paging.Page,
 ) (*apimodel.PageableResponse, gtserror.WithCode) {
+	// Mastodon API doesn't allow returning both resolved and unresolved reports in one query.
+	// Introduced an `unresolved` param to allow requesting both.
+	var resolved *bool
+	switch {
+	case resolvedParam && unresolvedParam:
+		resolved = nil
+	case resolvedParam:
+		resolved = util.Ptr(true)
+	case unresolvedParam:
+	case !resolvedParam && !unresolvedParam:
+		resolved = util.Ptr(false)
+	}
+
 	reports, err := p.state.DB.GetReports(
 		ctx,
 		resolved,
@@ -78,9 +92,12 @@ func (p *Processor) ReportsGet(
 	}
 
 	// Assemble next/prev page queries.
-	query := make(url.Values, 3)
-	if resolved != nil {
-		query.Set(apiutil.ResolvedKey, strconv.FormatBool(*resolved))
+	query := make(url.Values, 4)
+	if resolvedParam {
+		query.Set(apiutil.ResolvedKey, "true")
+	}
+	if unresolvedParam {
+		query.Set(apiutil.UnresolvedKey, "true")
 	}
 	if accountID != "" {
 		query.Set(apiutil.AccountIDKey, accountID)

docs/api/swagger.yaml
@@ -9296,9 +9296,13 @@ paths:
                 ````
             operationId: adminReports
             parameters:
-                - description: If set to true, only resolved reports will be returned. If false, only unresolved reports will be returned. If unset, reports will not be filtered on their resolved status.
+                - description: If present, resolved reports will be returned. If not present, unresolved reports will be returned.
                   in: query
                   name: resolved
+                  type: boolean
+                - description: If present, unresolved reports will always be returned. If not present, unresolved reports will be returned only if the resolved parameter is not present. Can be used with `resolved` to return both resolved and unresolved reports in the same query.
+                  in: query
+                  name: unresolved
                   type: boolean
                 - description: Return only reports created by the given account id.
                   in: query

internal/api/client/admin/reportsget.go
@@ -53,9 +53,16 @@ import (
 //		name: resolved
 //		type: boolean
 //		description: >-
-//			If set to true, only resolved reports will be returned.
-//			If false, only unresolved reports will be returned.
-//			If unset, reports will not be filtered on their resolved status.
+//			If present, resolved reports will be returned.
+//			If not present, unresolved reports will be returned.
+//		in: query
+//	-
+//		name: unresolved
+//		type: boolean
+//		description: >-
+//			If present, unresolved reports will always be returned.
+//			If not present, unresolved reports will be returned only if the resolved parameter is not present.
+//			Can be used with `resolved` to return both resolved and unresolved reports in the same query.
 //		in: query
 //	-
 //		name: account_id
@@ -154,7 +161,13 @@ func (m *Module) ReportsGETHandler(c *gin.Context) {
 		return
 	}
 
-	resolved, errWithCode := apiutil.ParseResolved(c.Query(apiutil.ResolvedKey), nil)
+	resolved, errWithCode := apiutil.ParseAdminResolved(c.Query(apiutil.ResolvedKey))
+	if errWithCode != nil {
+		apiutil.ErrorHandler(c, errWithCode, m.processor.InstanceGetV1)
+		return
+	}
+
+	unresolved, errWithCode := apiutil.ParseAdminUnresolved(c.Query(apiutil.UnresolvedKey))
 	if errWithCode != nil {
 		apiutil.ErrorHandler(c, errWithCode, m.processor.InstanceGetV1)
 		return
@@ -174,6 +187,7 @@ func (m *Module) ReportsGETHandler(c *gin.Context) {
 		c.Request.Context(),
 		authed.Account,
 		resolved,
+		unresolved,
 		c.Query(apiutil.AccountIDKey),
 		c.Query(apiutil.TargetAccountIDKey),
 		page,

internal/api/client/admin/reportsget_test.go
@@ -48,6 +48,7 @@ func (suite *ReportsGetTestSuite) getReports(
 	expectedHTTPStatus int,
 	expectedBody string,
 	resolved *bool,
+	unresolved *bool,
 	accountID string,
 	targetAccountID string,
 	maxID string,
@@ -67,6 +68,9 @@ func (suite *ReportsGetTestSuite) getReports(
 	requestPath := admin.ReportsPath + "?" + apiutil.LimitKey + "=" + strconv.Itoa(limit)
 	if resolved != nil {
 		requestPath = requestPath + "&" + apiutil.ResolvedKey + "=" + strconv.FormatBool(*resolved)
+	}
+	if unresolved != nil {
+		requestPath = requestPath + "&" + apiutil.UnresolvedKey + "=" + strconv.FormatBool(*unresolved)
 	}
 	if accountID != "" {
 		requestPath = requestPath + "&" + apiutil.AccountIDKey + "=" + accountID
@@ -128,8 +132,10 @@ func (suite *ReportsGetTestSuite) TestReportsGetAll() {
 	testAccount := suite.testAccounts["admin_account"]
 	testToken := suite.testTokens["admin_account"]
 	testUser := suite.testUsers["admin_account"]
+	resolved := util.Ptr(true)
+	unresolved := util.Ptr(true)
 
-	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", nil, "", "", "", "", "", 20)
+	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", resolved, unresolved, "", "", "", "", "", 20)
 	suite.NoError(err)
 	suite.NotEmpty(reports)
 
@@ -623,7 +629,7 @@ func (suite *ReportsGetTestSuite) TestReportsGetAll() {
   }
 ]`, string(b))
 
-	suite.Equal(`<http://localhost:8080/api/v1/admin/reports?limit=20&max_id=01GP3AWY4CRDVRNZKW0TEAMB5R>; rel="next", <http://localhost:8080/api/v1/admin/reports?limit=20&min_id=01GP3DFY9XQ1TJMZT5BGAZPXX7>; rel="prev"`, link)
+	suite.Equal(`<http://localhost:8080/api/v1/admin/reports?limit=20&max_id=01GP3AWY4CRDVRNZKW0TEAMB5R&resolved=true&unresolved=true>; rel="next", <http://localhost:8080/api/v1/admin/reports?limit=20&min_id=01GP3DFY9XQ1TJMZT5BGAZPXX7&resolved=true&unresolved=true>; rel="prev"`, link)
 }
 
 func (suite *ReportsGetTestSuite) TestReportsGetCreatedByAccount() {
@@ -632,7 +638,7 @@ func (suite *ReportsGetTestSuite) TestReportsGetCreatedByAccount() {
 	testUser := suite.testUsers["admin_account"]
 	account := suite.testAccounts["local_account_2"]
 
-	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", nil, account.ID, "", "", "", "", 20)
+	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", nil, nil, account.ID, "", "", "", "", 20)
 	suite.NoError(err)
 	suite.NotEmpty(reports)
 
@@ -895,7 +901,7 @@ func (suite *ReportsGetTestSuite) TestReportsGetTargetAccount() {
 	testUser := suite.testUsers["admin_account"]
 	targetAccount := suite.testAccounts["remote_account_1"]
 
-	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", nil, "", targetAccount.ID, "", "", "", 20)
+	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", nil, nil, "", targetAccount.ID, "", "", "", 20)
 	suite.NoError(err)
 	suite.NotEmpty(reports)
 
@@ -1156,10 +1162,267 @@ func (suite *ReportsGetTestSuite) TestReportsGetResolvedTargetAccount() {
 	testAccount := suite.testAccounts["admin_account"]
 	testToken := suite.testTokens["admin_account"]
 	testUser := suite.testUsers["admin_account"]
-	resolved := util.Ptr(false)
+	targetAccount := suite.testAccounts["local_account_2"]
+	resolved := util.Ptr(true)
+
+	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", resolved, nil, "", targetAccount.ID, "", "", "", 20)
+	suite.NoError(err)
+	suite.Len(reports, 1)
+
+	b, err := json.MarshalIndent(&reports, "", "  ")
+	suite.NoError(err)
+
+	suite.Equal(`[
+  {
+    "id": "01GP3DFY9XQ1TJMZT5BGAZPXX7",
+    "action_taken": true,
+    "action_taken_at": "2022-05-15T15:01:56.000Z",
+    "category": "other",
+    "comment": "this is a turtle, not a person, therefore should not be a poster",
+    "forwarded": true,
+    "created_at": "2022-05-15T14:20:12.000Z",
+    "updated_at": "2022-05-15T14:20:12.000Z",
+    "account": {
+      "id": "01F8MH5ZK5VRH73AKHQM6Y9VNX",
+      "username": "foss_satan",
+      "domain": "fossbros-anonymous.io",
+      "created_at": "2021-09-26T10:52:36.000Z",
+      "email": "",
+      "ip": null,
+      "ips": [],
+      "locale": "",
+      "invite_request": null,
+      "role": {
+        "id": "user",
+        "name": "user",
+        "color": "",
+        "permissions": "0",
+        "highlighted": false
+      },
+      "confirmed": false,
+      "approved": false,
+      "disabled": false,
+      "silenced": false,
+      "suspended": false,
+      "account": {
+        "id": "01F8MH5ZK5VRH73AKHQM6Y9VNX",
+        "username": "foss_satan",
+        "acct": "foss_satan@fossbros-anonymous.io",
+        "display_name": "big gerald",
+        "locked": false,
+        "discoverable": true,
+        "indexable": true,
+        "noindex": false,
+        "bot": false,
+        "created_at": "2021-09-26T10:52:36.000Z",
+        "note": "i post about like, i dunno, stuff, or whatever!!!!",
+        "url": "http://fossbros-anonymous.io/@foss_satan",
+        "avatar": "",
+        "avatar_static": "",
+        "header": "http://localhost:8080/assets/default_header.webp",
+        "header_static": "http://localhost:8080/assets/default_header.webp",
+        "header_description": "Flat gray background (default header).",
+        "followers_count": 0,
+        "following_count": 0,
+        "statuses_count": 4,
+        "last_status_at": "2024-11-01",
+        "emojis": [],
+        "fields": [],
+        "group": false
+      }
+    },
+    "target_account": {
+      "id": "01F8MH5NBDF2MV7CTC4Q5128HF",
+      "username": "1happyturtle",
+      "domain": null,
+      "created_at": "2022-06-04T13:12:00.000Z",
+      "email": "tortle.dude@example.org",
+      "ip": null,
+      "ips": [],
+      "locale": "en",
+      "invite_request": null,
+      "role": {
+        "id": "user",
+        "name": "user",
+        "color": "",
+        "permissions": "0",
+        "highlighted": false
+      },
+      "confirmed": true,
+      "approved": true,
+      "disabled": false,
+      "silenced": false,
+      "suspended": false,
+      "account": {
+        "id": "01F8MH5NBDF2MV7CTC4Q5128HF",
+        "username": "1happyturtle",
+        "acct": "1happyturtle",
+        "display_name": "happy little turtle :3",
+        "locked": true,
+        "discoverable": false,
+        "indexable": false,
+        "noindex": true,
+        "bot": false,
+        "created_at": "2022-06-04T13:12:00.000Z",
+        "note": "\u003cp\u003ei post about things that concern me\u003c/p\u003e",
+        "url": "http://localhost:8080/@1happyturtle",
+        "avatar": "",
+        "avatar_static": "",
+        "header": "http://localhost:8080/assets/default_header.webp",
+        "header_static": "http://localhost:8080/assets/default_header.webp",
+        "header_description": "Flat gray background (default header).",
+        "followers_count": 1,
+        "following_count": 1,
+        "statuses_count": 10,
+        "last_status_at": "2026-01-01",
+        "emojis": [],
+        "fields": [
+          {
+            "name": "should you follow me?",
+            "value": "maybe!",
+            "verified_at": null
+          },
+          {
+            "name": "age",
+            "value": "120",
+            "verified_at": null
+          }
+        ],
+        "hide_collections": true,
+        "group": false
+      },
+      "created_by_application_id": "01F8MGY43H3N2C8EWPR2FPYEXG"
+    },
+    "assigned_account": {
+      "id": "01F8MH17FWEB39HZJ76B6VXSKF",
+      "username": "admin",
+      "domain": null,
+      "created_at": "2022-05-17T13:10:59.000Z",
+      "email": "admin@example.org",
+      "ip": null,
+      "ips": [],
+      "locale": "en",
+      "invite_request": null,
+      "role": {
+        "id": "admin",
+        "name": "admin",
+        "color": "",
+        "permissions": "546033",
+        "highlighted": true
+      },
+      "confirmed": true,
+      "approved": true,
+      "disabled": false,
+      "silenced": false,
+      "suspended": false,
+      "account": {
+        "id": "01F8MH17FWEB39HZJ76B6VXSKF",
+        "username": "admin",
+        "acct": "admin",
+        "display_name": "",
+        "locked": false,
+        "discoverable": true,
+        "indexable": true,
+        "noindex": false,
+        "bot": false,
+        "created_at": "2022-05-17T13:10:59.000Z",
+        "note": "",
+        "url": "http://localhost:8080/@admin",
+        "avatar": "",
+        "avatar_static": "",
+        "header": "http://localhost:8080/assets/default_header.webp",
+        "header_static": "http://localhost:8080/assets/default_header.webp",
+        "header_description": "Flat gray background (default header).",
+        "followers_count": 1,
+        "following_count": 1,
+        "statuses_count": 4,
+        "last_status_at": "2021-10-20",
+        "emojis": [],
+        "fields": [],
+        "enable_rss": true,
+        "roles": [
+          {
+            "id": "admin",
+            "name": "admin",
+            "color": ""
+          }
+        ],
+        "group": false
+      },
+      "created_by_application_id": "01F8MGXQRHYF5QPMTMXP78QC2F"
+    },
+    "action_taken_by_account": {
+      "id": "01F8MH17FWEB39HZJ76B6VXSKF",
+      "username": "admin",
+      "domain": null,
+      "created_at": "2022-05-17T13:10:59.000Z",
+      "email": "admin@example.org",
+      "ip": null,
+      "ips": [],
+      "locale": "en",
+      "invite_request": null,
+      "role": {
+        "id": "admin",
+        "name": "admin",
+        "color": "",
+        "permissions": "546033",
+        "highlighted": true
+      },
+      "confirmed": true,
+      "approved": true,
+      "disabled": false,
+      "silenced": false,
+      "suspended": false,
+      "account": {
+        "id": "01F8MH17FWEB39HZJ76B6VXSKF",
+        "username": "admin",
+        "acct": "admin",
+        "display_name": "",
+        "locked": false,
+        "discoverable": true,
+        "indexable": true,
+        "noindex": false,
+        "bot": false,
+        "created_at": "2022-05-17T13:10:59.000Z",
+        "note": "",
+        "url": "http://localhost:8080/@admin",
+        "avatar": "",
+        "avatar_static": "",
+        "header": "http://localhost:8080/assets/default_header.webp",
+        "header_static": "http://localhost:8080/assets/default_header.webp",
+        "header_description": "Flat gray background (default header).",
+        "followers_count": 1,
+        "following_count": 1,
+        "statuses_count": 4,
+        "last_status_at": "2021-10-20",
+        "emojis": [],
+        "fields": [],
+        "enable_rss": true,
+        "roles": [
+          {
+            "id": "admin",
+            "name": "admin",
+            "color": ""
+          }
+        ],
+        "group": false
+      },
+      "created_by_application_id": "01F8MGXQRHYF5QPMTMXP78QC2F"
+    },
+    "statuses": [],
+    "rules": [],
+    "action_taken_comment": "user was warned not to be a turtle anymore"
+  }
+]`, string(b))
+	suite.Equal(`<http://localhost:8080/api/v1/admin/reports?limit=20&max_id=01GP3DFY9XQ1TJMZT5BGAZPXX7&resolved=true&target_account_id=01F8MH5NBDF2MV7CTC4Q5128HF>; rel="next", <http://localhost:8080/api/v1/admin/reports?limit=20&min_id=01GP3DFY9XQ1TJMZT5BGAZPXX7&resolved=true&target_account_id=01F8MH5NBDF2MV7CTC4Q5128HF>; rel="prev"`, link)
+}
+func (suite *ReportsGetTestSuite) TestReportsGetUnresolvedTargetAccount() {
+	testAccount := suite.testAccounts["admin_account"]
+	testToken := suite.testTokens["admin_account"]
+	testUser := suite.testUsers["admin_account"]
 	targetAccount := suite.testAccounts["local_account_2"]
 
-	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", resolved, "", targetAccount.ID, "", "", "", 20)
+	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", nil, nil, "", targetAccount.ID, "", "", "", 20)
 	suite.NoError(err)
 	suite.Empty(reports)
 
@@ -1175,7 +1438,7 @@ func (suite *ReportsGetTestSuite) TestReportsGetNotAdmin() {
 	testToken := suite.testTokens["local_account_1"]
 	testUser := suite.testUsers["local_account_1"]
 
-	reports, _, err := suite.getReports(testAccount, testToken, testUser, http.StatusForbidden, `{"error":"Forbidden: token has insufficient scope permission"}`, nil, "", "", "", "", "", 20)
+	reports, _, err := suite.getReports(testAccount, testToken, testUser, http.StatusForbidden, `{"error":"Forbidden: token has insufficient scope permission"}`, nil, nil, "", "", "", "", "", 20)
 	suite.NoError(err)
 	suite.Empty(reports)
 }
@@ -1184,26 +1447,30 @@ func (suite *ReportsGetTestSuite) TestReportsGetZeroLimit() {
 	testAccount := suite.testAccounts["admin_account"]
 	testToken := suite.testTokens["admin_account"]
 	testUser := suite.testUsers["admin_account"]
+	resolved := util.Ptr(true)
+	unresolved := util.Ptr(true)
 
-	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", nil, "", "", "", "", "", 0)
+	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", resolved, unresolved, "", "", "", "", "", 0)
 	suite.NoError(err)
 	suite.Len(reports, 2)
 
 	// Limit in Link header should be set to default (20)
-	suite.Equal(`<http://localhost:8080/api/v1/admin/reports?limit=20&max_id=01GP3AWY4CRDVRNZKW0TEAMB5R>; rel="next", <http://localhost:8080/api/v1/admin/reports?limit=20&min_id=01GP3DFY9XQ1TJMZT5BGAZPXX7>; rel="prev"`, link)
+	suite.Equal(`<http://localhost:8080/api/v1/admin/reports?limit=20&max_id=01GP3AWY4CRDVRNZKW0TEAMB5R&resolved=true&unresolved=true>; rel="next", <http://localhost:8080/api/v1/admin/reports?limit=20&min_id=01GP3DFY9XQ1TJMZT5BGAZPXX7&resolved=true&unresolved=true>; rel="prev"`, link)
 }
 
 func (suite *ReportsGetTestSuite) TestReportsGetHighLimit() {
 	testAccount := suite.testAccounts["admin_account"]
 	testToken := suite.testTokens["admin_account"]
 	testUser := suite.testUsers["admin_account"]
+	resolved := util.Ptr(true)
+	unresolved := util.Ptr(true)
 
-	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", nil, "", "", "", "", "", 2000)
+	reports, link, err := suite.getReports(testAccount, testToken, testUser, http.StatusOK, "", resolved, unresolved, "", "", "", "", "", 2000)
 	suite.NoError(err)
 	suite.Len(reports, 2)
 
 	// Limit in Link header should be set to 100
-	suite.Equal(`<http://localhost:8080/api/v1/admin/reports?limit=100&max_id=01GP3AWY4CRDVRNZKW0TEAMB5R>; rel="next", <http://localhost:8080/api/v1/admin/reports?limit=100&min_id=01GP3DFY9XQ1TJMZT5BGAZPXX7>; rel="prev"`, link)
+	suite.Equal(`<http://localhost:8080/api/v1/admin/reports?limit=100&max_id=01GP3AWY4CRDVRNZKW0TEAMB5R&resolved=true&unresolved=true>; rel="next", <http://localhost:8080/api/v1/admin/reports?limit=100&min_id=01GP3DFY9XQ1TJMZT5BGAZPXX7&resolved=true&unresolved=true>; rel="prev"`, link)
 }
 
 func TestReportsGetTestSuite(t *testing.T) {

internal/api/util/parsequery.go
@@ -46,6 +46,7 @@ const (
 	AccountIDKey       = "account_id"
 	TargetAccountIDKey = "target_account_id"
 	ResolvedKey        = "resolved"
+	UnresolvedKey      = "unresolved"
 	OffsetKey          = "offset"
 	DomainKey          = "domain"
 	OrderKey           = "order"
@@ -210,6 +211,14 @@ func ParseAdminStaff(value string, defaultValue bool) (bool, gtserror.WithCode) 
 
 func ParseAdminWithErrorsOnly(value string, defaultValue bool) (bool, gtserror.WithCode) {
 	return parseBool(value, defaultValue, AdminWithErrorsOnlyKey)
+}
+
+func ParseAdminResolved(value string) (bool, gtserror.WithCode) {
+	return parsePresent(value)
+}
+
+func ParseAdminUnresolved(value string) (bool, gtserror.WithCode) {
+	return parsePresent(value)
 }
 
 func ParseInteractionFavourites(value string, defaultValue bool) (bool, gtserror.WithCode) {
@@ -385,6 +394,14 @@ func parseInt(value string, defaultValue int, max int, min int, key string) (int
 	}
 
 	return i, nil
+}
+
+func parsePresent(value string) (bool, gtserror.WithCode) {
+	if value == "" {
+		return false, nil
+	}
+
+	return true, nil
 }
 
 // parseError returns gtserror.WithCode set to 400 Bad Request, to indicate

web/source/settings/lib/types/report.ts
@@ -110,9 +110,14 @@ export interface AdminReportResolveParams {
  */
 export interface AdminSearchReportParams {
 	/**
-	 * If set, show only resolved (true) or only unresolved (false) reports.
+	 * If set, show resolved reports. If unset, show unresolved reports.
+	 * Use both `resolved` and `unresolved` to show both resolved and unresolved reports in the same query.
 	 */
 	resolved?: boolean;
+	/**
+	 * If set, unresolved reports will always be shown.
+	 */
+	unresolved?: boolean;
 	/**
 	 * If set, show only reports created by the given account ID.
 	 */

web/source/settings/views/moderation/reports/search.tsx
@@ -76,7 +76,17 @@ function ReportSearchForm() {
 	// search (just show unresolved reports).
 	useEffect(() => {
 		if (hasParams) {
-			searchReports(Object.fromEntries(urlQueryParams));
+			const params = Object.fromEntries(urlQueryParams);
+			const resolved = form.resolved.value;
+			if (resolved === "true") {
+				params.resolved = "true";
+			} else if (resolved === "false") {
+				delete params.resolved;
+			} else {
+				params.resolved = "true";
+				params.unresolved = "true";
+			}
+			searchReports(params);
 		} else {
 			setResolved("false");
 			setLocation(location + "?resolved=false");

commit ace81a2480e6101edfb6a6e6563eccda979e55c3

[bugfix] Fix `GET /api/v1/admin/reports ` for explicitly provided `unresolved` param (#4977)
Fixes a bug I introduced in #4778 in my `unresolved` param implementation because of not knowing how switch statements work in golang. This affects client implementations of Mastodon admin API that use `unresolved` and would probably affect the GoToSocial dashboard in case someone has more than a single page of unresvoled reports (because `resolved` and `unresolved` params are explicitly provided in pagination prev/next links)

Removed a comment that's not relevant anymore, as Mastodon now understands `unresolved` as well.

> i thought i'm the only fedi dev who knows how to implement this specific api right. apparently there's not even a single one.
> [~me](https://social.hackerspace.pl/@mkljczk/117011080259158613)

Reviewed-on: https://codeberg.org/superseriousbusiness/gotosocial/pulls/4977

internal/processing/admin/report.go
@@ -46,8 +46,6 @@ func (p *Processor) ReportsGet(
 	targetAccountID string,
 	page *paging.Page,
 ) (*apimodel.PageableResponse, gtserror.WithCode) {
-	// Mastodon API doesn't allow returning both resolved and unresolved reports in one query.
-	// Introduced an `unresolved` param to allow requesting both.
 	var resolved *bool
 	switch {
 	case resolvedParam && unresolvedParam:
@@ -55,6 +53,7 @@ func (p *Processor) ReportsGet(
 	case resolvedParam:
 		resolved = util.Ptr(true)
 	case unresolvedParam:
+		resolved = util.Ptr(false)
 	case !resolvedParam && !unresolvedParam:
 		resolved = util.Ptr(false)
 	}